1a35719f6546e1c13cdf46f622ac44656028e34b
alcortazzo <42177346+alcortazzo@users.noreply.github.com>
2022-08-02 20:57:15 +0300

    Code refactoring

--- dht11_to_tg/__main__.py
+++ dht11_to_tg/__main__.py
@@ -1,4 +1,5 @@
 import time
+from typing import Optional
 
 import Adafruit_DHT as dht
 from telebot import TeleBot
@@ -6,7 +7,7 @@ from telebot import TeleBot
 from dht11_to_tg import PIN, SEND_ERROR_MESSAGE, SENSOR, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, TIME_TO_SLEEP
 
 
-def make_measurement() -> tuple[float, float]:
+def make_measurement() -> tuple[Optional[float], Optional[float]]:
     humidity, temperature = dht.read_retry(SENSOR, PIN)
     return humidity, temperature
 
@@ -22,14 +23,16 @@ if __name__ == "__main__":
         message: str = ""
         try:
             humidity, temperature = make_measurement()
-            print(f"Temperature: {temperature}°C Humidity: {humidity}%")
-            message = f"<b>Temperature:</b> {temperature}°C\n<b>Humidity:</b> {humidity}%"
-            send_message_to_tg(message)
+            if humidity is not None and temperature is not None:
+                print(f"Temperature: {temperature}°C Humidity: {humidity}%")
+                message = f"<b>Temperature:</b> {temperature}°C\n<b>Humidity:</b> {humidity}%"
+                send_message_to_tg(message)
+                time.sleep(TIME_TO_SLEEP)
+            else:
+                time.sleep(1)
         except KeyboardInterrupt:
             print("\nExiting...")
         except Exception as e:
             if SEND_ERROR_MESSAGE:
                 send_message_to_tg(str(e))
             print(e)
-
-        time.sleep(TIME_TO_SLEEP)
